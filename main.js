707e6706eceb8edf76cb915abaa8cd6f7be9c08c
Wladimir Guevara <wladige@gmail.com>
2020-12-30 20:27:52 -0300

    finzalizando ventana modal

--- main.js
+++ main.js
@@ -31,6 +31,22 @@ window.addEventListener('load', () => {
     document.querySelector('#barra-busqueda').addEventListener('input', (e) => {
         const busqueda = e.target.value;
         // console.log(busqueda);
-grid.filter((item) => item.getElement().dataset.etiquetas.includes(busqueda) ); 
+        grid.filter((item) => item.getElement().dataset.etiquetas.includes(busqueda));
+    });
+    // listener para las imagenes
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.grid .item img').forEach((ele) => {
+        // console.log(ruta);
+        ele.addEventListener('click', () => {
+            const ruta = ele.getAttribute('src');
+            const descripcion = ele.parentNode.parentNode.dataset.descripcion;
+            overlay.classList.add('activo');
+            document.querySelector('#overlay img').src = ruta;
+            document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+        });
+    });
+    // evento listener para el boton de cerrar
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', ()=>{
+        overlay.classList.remove('activo');
     });
 });
